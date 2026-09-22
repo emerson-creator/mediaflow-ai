87315d2c784051bcd2566b9affddec0b299802c3
emerson-creator <emerson.albornoz@correounivalle.edu.co>
2026-09-22 08:06:58 -0500

    feat: ingestion set up

--- services/ingestion/src/app.module.ts
+++ services/ingestion/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { MediaModule } from './media/media.module.js';
+import { MessagingModule } from './messaging/messaging.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -14,6 +16,8 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'ingestion',
     }),
+    MediaModule,
+    MessagingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ services/ingestion/src/media/media.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { MediaController } from './media.controller.js';
+import { MediaService } from './media.service.js';
+
+describe('MediaController', () => {
+  let controller: MediaController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [MediaController],
+      providers: [MediaService],
+    }).compile();
+
+    controller = module.get<MediaController>(MediaController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
